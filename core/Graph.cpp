e5a18e4a94ca574781d47ecf3883f5d706de9a0b
Jason Stockwell <jstock23@gmail.com>
2024-02-24 11:59:57 -0800

    Update

--- core/Graph.cpp
+++ core/Graph.cpp
@@ -1,5 +1,15 @@
 #include "graph.h"
 
+Graph::Graph() {
+
+    Node* everything = new Node("1");
+    nodes.push_back("1", everything);
+    Node* nothing = new Node("0");
+    nodes.push_back("0", nothing);
+
+
+}
+
 Graph::~Graph() {
     // Clean up allocated nodes and edges
 }
--- core/Graph.h
+++ core/Graph.h
@@ -12,18 +12,18 @@ private:
    
 
 public:
-    Graph() {
 
- /*
-    
+    /*
         Root node could have UID 1, which represents Everything.
         Potentially have Nothing node with UID 0.
-    
     */
-
-    }
+    Graph();
     ~Graph();
-    
+
+    /*
+        New nodes are added to the graph as subsets of an existing graph or Everything by default.
+        All nodes need a superset node or else they are not reachable from Everything.
+    */
     void addNode(Node* node);
     void addEdge(Edge* edge);
     void removeNode(std::string nodeId);
