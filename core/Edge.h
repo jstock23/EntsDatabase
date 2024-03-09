61aeba56d9e26fbf7dcec4fb1bc69cbc608c829a
Connor Trump <trumpconnor@gmail.com>
2024-03-09 10:35:41 -0800

    making sure Graph.cpp and Graph.h are updated

--- core/Edge.h
+++ core/Edge.h
@@ -5,20 +5,36 @@
 #include "Node.h"
 
 class Edge {
-public:
-
+private:
     /*
         There are 2 types of edges in the graph. Edges connecting supersets to subsets are directed from the superset to the subset.
+        Superset and subsets are represeted by "^" symbol.
         Edges connecting two disjoint sets are undirected.
+        Disjoints sets are represented by "!" symbol.
+        There should be edges implying continual hypernym inheritance to the bottom of the.
+        There should be edges implying single layer hypernym inheritance.
     */
 
     // I changed the edge constructor to take Node* objects instead of strings.
     // Each edge should be created between two Node* objects (Pointers to nodes)
-    Node* from;
-    Node* to;
-    
-    Edge(Node* uidFrom, Node* uidTo) : from(uidFrom), to(uidTo) {}
-      
+    Node* source;
+    Node* target;
+    std::string type;
+
+public:
+    Edge(Node* source, Node* target, std::string type);
+    ~Edge();
+
+    // Setters and Getters
+    void setSource(Node* source);
+    Node* getSource() const;
+
+    void setTarget(Node* target);
+    Node* getTarget() const;
+
+    void setType(std::string type);
+    std::string getType() const;
+
 };
 
 #endif // EDGE_H
--- core/Node.cpp
+++ core/Node.cpp
@@ -1,19 +1,53 @@
-
-
 // Node.cpp
 #include "Node.h"
+//#include "Graph.h"
 
-Node::Node() {
-    // Constructor implementation
-    std::string id = "0";
-    std::set<Node*> superset;
-    std::set<Node*> subset;
-    Graph* definition;
-    
+Node::Node(const std::string& id, const std::string& name)
+    : id(id), name(name), {
 }
 
 Node::~Node() {
-    // Destructor implementation
 }
 
-// Implement other member functions here
+// Setters
+void Node::setId(const std::string& newId) {
+    id = newId;
+}
+
+void Node::setName(const std::string& newName) {
+    name = newName;
+}
+
+// Getters
+const std::string& Node::getId() const {
+    return id;
+}
+
+const std::string& Node::getName() const {
+    return name;
+}
+
+const std::set<Node*>& Node::getSuperset() const {
+    return superset;
+}
+
+const std::set<Node*>& Node::getSubset() const {
+    return subset;
+}
+
+// Relationship Management
+void Node::addSupersetNode(Node* node) {
+    superset.insert(node);
+}
+
+void Node::removeSupersetNode(Node* node) {
+    superset.erase(node);
+}
+
+void Node::addSubsetNode(Node* node) {
+    subset.insert(node);
+}
+
+void Node::removeSubsetNode(Node* node) {
+    subset.erase(node);
+}
